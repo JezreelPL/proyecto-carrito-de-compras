c267d7dbf448e07fe5e41b338f9aaaa43c5c4b5a
JezreelPL <jezreelpulop8@gmail.com>
2026-02-09 23:08:25 -0600

    agregue la viaulizacion de los cursos en el HTML

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 //asingancion a variables
 const carrito = document.querySelector('#carrito');
-//generamos HTMl con la etiqueta  <tbody></tbody> servira para colocar los elementos
-const contenedorCarrito = document.querySelector('#lista-cursos tbody');
+//generamos HTMl con la etiqueta  <tbody></tbody> servira para colocar los elementos nuevos
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
-
+/*Arreglo para llenarlo  de los cursos */
+let articulosCarrito = [];
 
 cargarEventListeners();
 
@@ -25,7 +26,7 @@ function agregarCurso(e){
 
 //lee el contenido del HTML que le dimos clik y extrae la informacion del curso
 function leerDatosCurso(curso){
-console.log(curso);
+//   console.log(curso);
 /*Crear un objeto con el contenido del curso actual */
 const infoCurso = {
 imagen: curso.querySelector('img').src,
@@ -34,6 +35,39 @@ precio : curso.querySelector('.precio span').textContent,
 id: curso.querySelector('a').getAttribute('data-id'),
 cantidad: 1
 }
-console.log(infoCurso);
+
+/*Agregar elementos al arreglo de carrito*/
+articulosCarrito =[...articulosCarrito, infoCurso];
+console.log(articulosCarrito);
+carritoHTML();
 }
 
+/*Muestra el carrito en el HTML */
+function carritoHTML(){
+    //Limpiar el HMTL 
+    limpiarCurso();
+    //recorre el carrito y genera el HTML
+
+    articulosCarrito.forEach((curso) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+        ${curso.titulo}
+        </td>
+        `;
+        /* Agregar el HTML del carrito en el "tbody"*/
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+/*Eliminar el curso del tbody */
+function limpiarCurso(){
+    /*Esta es la forma lenta  
+    contenedorCarrito.innerHTML = '';*/
+    /*La mejor manera de limpiar el html es usar un while 
+    su explicacion facil es que elimina el hijo siguiente de la etique como un div*/
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+
+}
